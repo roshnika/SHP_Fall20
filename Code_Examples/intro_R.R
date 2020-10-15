b22ff223e775efcd96d107149d50985bb9f285ad
Owen Ward <33092609+OwenWard@users.noreply.github.com>
2020-10-15 19:13:01 -0400

    add some simple code examples

---
+++ Code_Examples/intro_R.R
@@ -0,0 +1,51 @@
+##### An introduction to programming in R
+
+
+### 1. Sign up to RStudio Cloud
+### 2. Set up a project using the Github repo.
+
+
+## This is an R script.
+## You can write code here.
+## To run a line of code
+## - Press Ctrl/Cmd + Enter and the line
+## your cursor is on will run
+## Can also highlight multiple lines and run them.
+## Can also type directly into the console.
+
+#### Task 1 ####
+## run the following line of code in the console
+## what is the output?
+print("Hello World")
+
+
+#### Task 2 #### 
+## Perform some basic calculations in the console
+## run these examples
+2.145 + 3.334
+0.12^10
+sqrt(10)
+sqrt(-1)
+
+#### Task 3 ####
+## Let's find out how to take the log of a real number in R.
+## Try get help on what functions R has for this by running
+?log
+## Compute the log of 12 to the base 4 and 27 to the base 3
+
+## Write the code for these here
+
+
+#### Task 4 ####
+## store your answers to the previous question in two objects,
+## calling them x and y
+## what is the result from running the below line
+
+x + y
+
+
+
+#### Task 5 ####
+## create a vector, z, using `c` with
+## two elements, the first being x and the second being y
+
--- Week1/week1.R
+++
@@ -1,1 +0,0 @@
-##### An introduction to programming in R
